e943da724c43792519467df821d78c89de5c1938
aniq321 <107672192+aniq321@users.noreply.github.com>
2022-11-24 09:07:08 +0800

    Delete padampekerja.php

--- padampekerja.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "COMPONENT/DB/connection.php";
-$id = $_GET['delete_id'];
-$result = mysqli_query($con,"DELETE FROM info_pekerja WHERE ID='$id'");
- echo "<script>window.location='index.php'</script>";
-?>
